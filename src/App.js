b5662933ab2cdf00333d5b518eb6b4c2ac4c2c33
Carlos <carlozalejandro98@gmail.com>
2022-01-01 11:26:23 -0500

    menu toggle finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import NavHeader from './components/NavbarApp/NavHeader'
 //import './styles/index.scss';
 import Footer from "./components/Footer/Footer"
 import Routing from './components/Routes/Routes';
+import './styles/transition.scss'
+
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false)
+  const [size, setSize] = useState({
+      width: undefined,
+      height: undefined,
+  });
+  
+  const menuToggleHandler = () => {
+      setMenuOpen((p) => !p);
+  }
+
+  useEffect(() =>{
+      const handleResize = () => {
+         setSize({
+             width: window.innerWidth,
+             height: window.innerHeight,
+         });
+      };
+      window.addEventListener("resize",handleResize);
+
+      return () => window.removeEventListener("resize", handleResize)
+  },[])
+
+  useEffect(() => {
+     if(size.width > 1059 && menuOpen) {
+         setMenuOpen(false)
+     }
+  }, [size.width, menuOpen]);
+
   return (
     <>
+    
       <div className="page-container">
-        <div className="content-wrap">
-          <NavHeader />
+      <NavHeader 
+      menuOpen={menuOpen}
+      menuToggleHandler={menuToggleHandler}
+      />
+      <div className={"page-container " + "transition " + (menuOpen?'hide':'')}>
           <Routing />
-        </div>
         <Footer />
+        </div>
       </div>
     </>
   );
--- src/components/NavbarApp/NavHeader.js
+++ src/components/NavbarApp/NavHeader.js
@@ -6,36 +6,9 @@ import {BiMenuAltRight} from "react-icons/bi"
 import {AiOutlineClose} from "react-icons/ai"
 import { Link } from "react-router-dom"
 
-const NavHeader = () => {
+const NavHeader = ({menuOpen, menuToggleHandler}) => {
 
-    const [menuOpen, setMenuOpen] = useState(false)
-    const [size, setSize] = useState({
-        width: undefined,
-        height: undefined,
-    });
-    
-    const menuToggleHandler = () => {
-        setMenuOpen((p) => !p);
-    }
-
-    useEffect(() =>{
-        const handleResize = () => {
-           setSize({
-               width: window.innerWidth,
-               height: window.innerHeight,
-           });
-        };
-        window.addEventListener("resize",handleResize);
-
-        return () => window.removeEventListener("resize", handleResize)
-    },[])
-
-    useEffect(() => {
-       if(size.width > 1059 && menuOpen) {
-           setMenuOpen(false)
-       }
-    }, [size.width, menuOpen]);
-
+   
     return (
       <>
         <header className={classes.header}>
@@ -48,19 +21,19 @@ const NavHeader = () => {
             >
               <ul>
                 <li>
-                  <Link to="/">Alojamiento</Link>
+                  <Link to="/" onClick={menuToggleHandler}>Alojamiento</Link>
                 </li>
                 <li>
-                  <Link to="/vuelos">Vuelos</Link>
+                  <Link to="/vuelos" onClick={menuToggleHandler}>Vuelos</Link>
                 </li>
                 <li>
-                  <Link to="/coches">Alquiler de Coches</Link>
+                  <Link to="/coches" onClick={menuToggleHandler}>Alquiler de Coches</Link>
                 </li>
                 <li>
-                  <Link to="/turismo">Atracciones Turisticas</Link>
+                  <Link to="/turismo" onClick={menuToggleHandler}>Atracciones Turisticas</Link>
                 </li>
                 <li>
-                  <Link to="/taxi">Taxi al Aereopuerto</Link>
+                  <Link to="/taxi" onClick={menuToggleHandler} >Taxi al Aereopuerto</Link>
                 </li>
               </ul>
             </nav>
