0283205efa08d17499a7939d39b659db556ea001
Carlos <carlozalejandro98@gmail.com>
2022-01-02 17:05:56 -0500

    some details changed because of problems at mobile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{useState, useEffect} from 'react';
 import NavHeader from './components/NavbarApp/NavHeader'
-//import './styles/index.scss';
+import './styles/index.scss';
 import Footer from "./components/Footer/Footer"
 import Routing from './components/Routes/Routes';
 import './styles/transition.scss'
